4ec16ee975fcb6b21206a96747eedb09b106a517
Sarah <sarahpu2009@gmail.com>
2020-05-25 01:53:19 -0500

    Update Tree.java

--- Boids/Tree.java
+++ Boids/Tree.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Tree here.
  * 
- * @author (your name) 
+ * @author Sarah Pu
  * @version (a version number or a date)
  */
 public class Tree  extends SmoothMover
